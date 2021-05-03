a364f871009c3592c90b393c03621bedefbd3e2f
Necmettin Şahinbay <sahinbaynecmeddin@gmail.com>
2021-05-04 01:21:39 +0300

    fix config

--- djdict/settings_prod.py
+++ djdict/settings_prod.py
@@ -88,9 +88,11 @@ EMAIL_HOST = os.environ.get("EMAIL_HOST")
 EMAIL_PORT = os.environ.get("EMAIL_PORT")
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
-EMAIL_USE_TLS = True
+EMAIL_FROM = os.environ.get("EMAIL_FROM")
+EMAIL_USE_TLS = os.environ.get("EMAIL_USE_TLS")
+EMAIL_USE_SSL = os.environ.get("EMAIL_USE_SSL")
 
-EMAIL_BACKEND = "djcelery_email.backends.CeleryEmailBackend"
+EMAIL_BACKEND = os.environ.get("EMAIL_BACKEND")
 CELERY_EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 
 
