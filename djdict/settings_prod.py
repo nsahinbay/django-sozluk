9fe483f98be5d495c72359336d9e926e6241fa94
Necmettin Şahinbay <sahinbaynecmeddin@gmail.com>
2021-05-04 01:32:58 +0300

    fix config

--- djdict/settings_prod.py
+++ djdict/settings_prod.py
@@ -89,7 +89,7 @@ EMAIL_PORT = os.environ.get("EMAIL_PORT")
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
 EMAIL_FROM = os.environ.get("EMAIL_FROM")
-EMAIL_USE_TLS = os.environ.get("EMAIL_USE_TLS")
+#EMAIL_USE_TLS = os.environ.get("EMAIL_USE_TLS")
 EMAIL_USE_SSL = os.environ.get("EMAIL_USE_SSL")
 
 EMAIL_BACKEND = os.environ.get("EMAIL_BACKEND")
